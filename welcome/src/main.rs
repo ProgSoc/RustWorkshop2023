efd7fa2cf28490a7f51093b4ee860a4a7ad81fda
Oliver Lenehan <oli@sunsetkookaburra.xyz>
2023-09-08 07:49:57 +1000

    feat: welcome with brief good, existing examples

--- welcome/src/main.rs
+++ welcome/src/main.rs
@@ -1,26 +1,78 @@
-use input_macro::input;
 use std::{thread::sleep, time::Duration};
 
+fn get_thing() -> i32 {
+  return 42;
+}
+
+/// Walk-through of a Wikipedia example, no-boilerplate example and more
 fn main() {
-    // Activity 1 (Playground)
+    // Activity
     // Compile a Rust program on your machine.
     println!("🤖 Beep...");
     println!("...Boop! 🤖");
     println!("Hello, World! 👋🌏\n");
 
-    // Activity 2 (Playground)
+    // Activity
+    // Variables
+    let x;
+    x = 4;
+    // Inline, not out of place in Javascript
+    let x = 42;
+    // Types, like Typescript
+    let x: i32;
+    x = 42;
+    let x: i32 = 42;
+    // i32 is a signed 32-bit integer
+    // if you've used C on a modern computer, that's int!
+    //
+    let x;
+    // this will work on C! but what does it do??
+    // println!(x);
+    x = 42;
+    // println!(x);
+    //
+    let _ = 42;
+    let _ = get_thing();
+
+    // Activity
+    // Control-Flow
+    // let values = vec![1, 2, 3, 4];
+    let mut values = vec![1, 2, 3, 4, 11];
+    //
+    // for value in values {
+    for value in &values {
+        println!("value = {}", value);
+    }
+    //
+    if values.len() > 5 {
+        println!("🦣 List is longer than five items");
+    }
+    //
+    // Pattern matching
+    match values.len() {
+        0 => println!("🕳️ Empty"),
+        1 => println!("🐪 One value"),
+        // pattern matching can use ranges of integers
+        2..=10 => println!("🐫 Between two and ten values"),
+        11 => println!("🦀 Eleven values"),
+        // A `_` pattern is called a "wildcard", it matches any value
+        _ => println!("✨ Many values"),
+    }
+    //
+    while let Some(value) = values.pop() {
+        println!("👋 Goodbye value = {value} !");
+    }
+
+    // Brief expressions
+    let x = {
+        let y = 1;
+        let z = 2;
+        y + z;
+    };
+
+    // Activity
     // Using more than what's already here (use std).
     println!("Yawwnnn 🥱 zzzzz 😴");
-    sleep(Duration::from_secs(3));
+    sleep(Duration::from_secs(5));
     println!("Oooh 🤭, hopefully I didn't miss anything 😊");
-
-    // Activity 3
-    // Use an external crate to add some interactivity.
-    println!("Hello, {}!", input!("What's your name? "));
-
-    // Activity 4
-    // Lightly using generics and results.
-    println!("Let's do some maths...");
-    let x: isize = input!("Give me a number to add to 2: ").parse().unwrap();
-    println!("2 + {x} = {}", 2 + x);
 }
